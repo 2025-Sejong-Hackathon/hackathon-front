27aef3b4613c31da2e3c1062fa9a31fe2d973dd5
unknown <jhje5595@naver.com>
2025-12-23 01:30:36 +0900

    chore: 초기 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useNavigate, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home.jsx';
+import MainLayout from './layouts/MainLayout.jsx';
+import AuthLayout from './layouts/AuthLayout.jsx';
+import Login from './pages/Login/Login.jsx';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
+  const naviate = useNavigate();
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docsdd'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path='/' element={<Home />} />
+          </Route>
+        </Routes>
+
+        <Routes>
+          <Route element={<AuthLayout />}>
+            <Route path='/login' element={<Login />} />
+          </Route>
+        </Routes>
+      </main>
     </>
   );
 }
-
-export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className='flex justify-center items-center text-blue-400'>
+      오랜만이다
+    </div>
+  );
+}
